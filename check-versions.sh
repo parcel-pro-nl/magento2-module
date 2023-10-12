05a2fa9ae2c30ca78df80bc27f02bf3b9a940240
Luca Scalzotto <luca@scorpiac.com>
2023-10-12 15:21:10 +0200

    Check version in system.xml

--- check-versions.sh
+++ check-versions.sh
@@ -21,6 +21,7 @@ echo "Detected versions:"
 composerVersion=$(jq -r .version 'composer.json')
 checkVersionMatch 'composer.json' "$composerVersion"
 
+checkVersionMatch 'etc/adminhtml/system.xml' "$(sed -nE 's/^.*<comment>(.*)<.*$/\1/p' 'etc/adminhtml/system.xml')"
 checkVersionMatch 'CHANGELOG.md' "$(sed -nE 's/^## (.*) -.*$/\1/p' 'CHANGELOG.md' | head -n 1)"
 
 if [ ! "$allMatch" = 1 ]
