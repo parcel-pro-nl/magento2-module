e207b0cc87b14b0935949962b1077968f3374366
Luca Scalzotto <luca@scorpiac.com>
2024-01-24 13:01:09 +0100

    Remove unused wrapping in set-shipping-information-mixin

--- view/frontend/web/js/action/set-shipping-information-mixin.js
+++ view/frontend/web/js/action/set-shipping-information-mixin.js
@@ -6,17 +6,13 @@
 define(
     [
         'Magento_Checkout/js/model/quote',
-        'mage/utils/wrapper',
         'Parcelpro_Shipment/js/model/shipping-save-processor'
     ],
-    function (quote, wrapper, shippingSaveProcessor) {
+    function (quote, shippingSaveProcessor) {
         'use strict';
 
-        return function (setShippingInformationAction) {
-
-            return wrapper.wrap(setShippingInformationAction, function (original) {
-                return shippingSaveProcessor.saveShippingInformation(quote.shippingAddress()?.getType());
-            });
+        return function () {
+            return shippingSaveProcessor.saveShippingInformation(quote.shippingAddress()?.getType());
         };
     }
 );
